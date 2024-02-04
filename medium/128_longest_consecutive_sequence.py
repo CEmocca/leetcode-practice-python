1a41fcadf9078cd456a71f4e0c03c5fc0993188a
DESKTOP-QRK32Q2\MoCca <alwaysm0cca@gmail.com>
2024-02-05 02:40:05 +0700

    add optimize solution for longest consecutive

--- medium/128_longest_consecutive_sequence.py
+++ medium/128_longest_consecutive_sequence.py
@@ -28,29 +28,64 @@ class Solution:
                 
         return ans
     
+    # time complexity = O(nlogn + n)
+    # space complexity = O(1)
     def longest_consecutive_sorted(self, nums: list[int]) -> int:
         if len(nums) == 0:
             return 0
         
-        sorted_array = sorted(nums, reverse=False)
-        print(sorted_array)
+        nums = sorted(nums, reverse=False)
         longest_consecutive = 1
         count = 1
         
-        for i, num in enumerate(sorted_array):
-            print(f'num - 1 = {num - 1} == {sorted_array[i - 1]}, count = {count}')
-            if num - 1 == sorted_array[i - 1]:
+        for i, num in enumerate(nums):
+            if num - 1 == nums[i - 1]:
                 count += 1
-            elif num == sorted_array[i - 1]:
+            elif num == nums[i - 1]:
                 continue
             else:
                 longest_consecutive = max(count, longest_consecutive)
                 count = 1
         return max(count, longest_consecutive)
-
+    
+    # time complexity = 
+    #   O(N) for building set
+    #   O(N) for iterate through every element once
+    #   O(N + N) = O(N)
+    
+    # space complextity = O(N)
+    def longest_consecutive_neetcode_2nd(self, nums: list[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        
+        set_array = set(nums)
+        
+        visited = set()
+        longest_consecutive = 1
+        count = 1
+        
+        for num in set_array:
+            if num in visited:
+                continue
+            
+            visited.add(num)
+            
+            i = 1
+            while num + i in set_array:
+                count += 1
+                visited.add(num + i)
+                i += 1
+                
+            
+            longest_consecutive = max(count, longest_consecutive)
+            count = 1
+            
+        return longest_consecutive
                 
     
 solution = Solution()
 
-print(solution.longest_consecutive_sorted([1,2,0,1]))
-print(solution.longest_consecutive_sorted([0,3,7,2,5,8,4,6,0,1]))
+print(solution.longest_consecutive_neetcode_2nd([1,2,0,1]))
+print(solution.longest_consecutive_neetcode_2nd([0,3,7,2,5,8,4,6,0,1]))
+print(solution.longest_consecutive_neetcode_2nd([100,4,200,1,3,2]))
+print(solution.longest_consecutive_neetcode_2nd([0, -1]))
